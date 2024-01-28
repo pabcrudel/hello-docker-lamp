9d11d2cca2aa80adf817ceecbd29108c06f96b4a
pabcrudel <pabcrudel@gmail.com>
2024-01-28 15:02:54 +0100

    add: Simple.css classless framework

--- website/index.php
+++ website/index.php
@@ -9,6 +9,7 @@ $title = 'Hello Docker LAMP';
   <meta charset="UTF-8">
   <title><?=$title?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://unpkg.com/simpledotcss/simple.min.css" />
 </head>
 
 <body>
