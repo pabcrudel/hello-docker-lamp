be6819624e114bb9e4863ef1f82f6cae399c8faf
pabcrudel <pabcrudel@gmail.com>
2024-01-28 14:59:17 +0100

    feature: Add PHP to the home page

--- website/index.php
+++ website/index.php
@@ -1,14 +1,18 @@
+<?php
+$title = 'Hello Docker LAMP';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
-  <title>Hello Docker LAMP</title>
+  <title><?=$title?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
-  <h1>Hello Docker LAMP</h1>
+  <h1><?=$title?></h1>
 </body>
 
 </html>
