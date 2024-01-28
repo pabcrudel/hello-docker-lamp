3369a421f317968d612bfcfa539771511a647679
pabcrudel <pabcrudel@gmail.com>
2024-01-28 19:19:53 +0100

    add: Connect to database and display all data

---
+++ website/blog.php
@@ -0,0 +1,40 @@
+<?php
+$connection = mysqli_connect(
+  'database', 'pabcrudel', 'password', 'pabcrudel_database'
+);
+$query = 'SELECT * FROM blog';
+$result = mysqli_query($connection, $query);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Blog</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://unpkg.com/simpledotcss/simple.min.css" />
+</head>
+
+<body>
+  <h1>Blog</h1>
+
+  <nav>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="info.php">Server info</a></li>
+    </ul>
+  </nav>
+
+  <?php
+  while($record = mysqli_fetch_assoc($result)) {
+    echo '<section>';
+    echo '<h2>'.$record['title'].'</h2>';
+    echo '<p>'.$record['content'].'</p>';
+    echo '<p>Posted on: '.$record['date'].'</p>';
+    echo '</section><hr>';
+  }
+  ?>
+</body>
+
+</html>
